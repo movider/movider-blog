6bf8b2049f5f198b2ba76a315a55dee455fe79b4
Chittinun chuwisetwanitch <Chittinun@1moby.com>
2019-06-04 11:02:16 +0700

    remove blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,7 +5,7 @@ import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 // import { rhythm, scale } from "../utils/typography"
-import { rhythm } from "../utils/typography"
+// import { rhythm } from "../utils/typography"
 
 class BlogPostTemplate extends React.Component {
   render() {
