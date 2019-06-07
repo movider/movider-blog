9c40b2fb700e6ad5f49885b6ef706fe232d2aca4
Phoowanon <phoowanon@1moby.com>
2019-06-07 15:24:25 +0700

    fix css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,54 +1,70 @@
 import React from "react"
 // import { Link } from "gatsby"
 
-import { rhythm, scale } from "../utils/typography"
+import {
+  rhythm,
+  scale
+} from "../utils/typography"
 
 class Layout extends React.Component {
   render() {
-    const { children } = this.props
+    const {
+      children
+    } = this.props
     // const { location, title, children } = this.props
     // const rootPath = `${__PATH_PREFIX__}/`
     let header
     // if (location.pathname === rootPath) {
 
-    // } 
+    // }
 
-    header = (
-      <h1
-        style={{
+    header = ( <
+      h1 style = {
+        {
           ...scale(1.5),
           marginBottom: rhythm(1.5),
           marginTop: 0,
           textAlign: "center"
-        }}
-      >
-        <a href="https://movider.co/blog/" style={{
+        }
+      } >
+      <
+      a href = "https://movider.co/blog/"
+      style = {
+        {
           boxShadow: "none",
           display: "block"
-        }}><img className="logo" src="/blog/images/movider_logo.svg" alt="logo" /></a>
-        {/* {title} */}
-      </h1 >
+        }
+      } > < img className = "logo"
+      src = "/blog/images/movider_logo.svg"
+      alt = "logo" / > < /a> {
+        /* {title} */ } <
+      /h1 >
     )
-    return (
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <header>{header}</header>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, <a href={`https://movider.co`} style={{
-            boxShadow: "none"
-          }}>
-            Movider.co
-              </a>
-          {` `}
-        </footer>
-      </div>
+    return ( <
+      div >
+      <
+      header > {
+        header
+      } < /header> <
+      main className = "main-width" > {
+        children
+      } < /main> <
+      footer > ©{
+        new Date().getFullYear()
+      }, < a href = {
+        `https://movider.co`
+      }
+      style = {
+        {
+          boxShadow: "none"
+        }
+      } >
+      Movider.co <
+      /a> {
+        ` `
+      } <
+      /footer> <
+      /div>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,73 +1,123 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import {
+  Link,
+  graphql
+} from "gatsby"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import {
+  rhythm
+} from "../utils/typography"
 
 class BlogIndex extends React.Component {
   render() {
-    const { data } = this.props
+    const {
+      data
+    } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="Movider" />
+    return ( <
+      Layout location = {
+        this.props.location
+      }
+      title = {
+        siteTitle
+      } >
+      <
+      SEO title = "Movider" / >
 
-        {posts.map(({ node }) => {
+      {
+        posts.map(({
+          node
+        }) => {
           const title = node.frontmatter.title || node.fields.slug
-          return (
-            <div key={node.fields.slug} className="moviderBorder moviCenter">
-              <Link to={node.fields.slug}>
-                <div className="picCover">
-                  <img src={node.frontmatter.thumbnail} alt="content" />
-                </div>
-              </Link>
-              <h2 className="titleContent"
-                style={{
-                  marginTop: "0.3rem",
-                  marginBottom: rhythm(1 / 4),
-                  textAlign: "center"
-                }}
-              >
-                <Link style={{
-                  boxShadow: `none`,
-                  color: "rgb(62, 70, 91)"
-                }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h2>
+          return ( <
+            div key = {
+              node.fields.slug
+            }
+            className = "moviderBorder moviCenter" >
+            <
+            Link to = {
+              node.fields.slug
+            } >
+            <
+            div className = "picCover" >
+            <
+            img src = {
+              node.frontmatter.thumbnail
+            }
+            alt = "content" / >
+            <
+            /div> <
+            /Link> <
+            h2 className = "titleContent"
+            style = {
+              {
+                marginTop: "0.3rem",
+                marginBottom: rhythm(1 / 4),
+                textAlign: "center"
+              }
+            } >
+            <
+            Link style = {
+              {
+                boxShadow: `none`,
+                color: "rgb(62, 70, 91)"
+              }
+            }
+            to = {
+              node.fields.slug
+            } > {
+              title
+            } <
+            /Link> <
+            /h2>
 
-              <div className="post_detail post_date">
-                <span className="post_info_date">
-                  <span>{node.frontmatter.date}</span>
-                </span>
-              </div>
+            <
+            div className = "post_detail post_date" >
+            <
+            span className = "post_info_date" >
+            <
+            span > {
+              node.frontmatter.date
+            } < /span> <
+            /span> <
+            /div>
 
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
+            <
+            p dangerouslySetInnerHTML = {
+              {
+                __html: node.frontmatter.description || node.excerpt,
+              }
+            }
+            />
 
-              <Link className="continueReading" to={node.fields.slug}>
-                Continue Reading →
-              </Link>
+            <
+            Link className = "continueReading"
+            to = {
+              node.fields.slug
+            } >
+            Continue Reading→ <
+            /Link>
 
-            </div>
+            <
+            /div>
           )
-        })}
-        <Bio />
-      </Layout>
+        })
+      } <
+      Bio / >
+      <
+      /Layout>
     )
   }
 }
 
 export default BlogIndex
 
-export const pageQuery = graphql`
+export const pageQuery = graphql `
   query {
     site {
       siteMetadata {
